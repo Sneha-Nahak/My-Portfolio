9e660320c732e4b1fc9a97a2700e029daa4d4edb
Sneha Nahak <snehanahak6@gmail.com>
2025-04-11 05:50:37 +0530

    updated the HackerRoom.jsx file

--- src/canvas/HackerRoom.jsx
+++ src/canvas/HackerRoom.jsx
@@ -4,7 +4,7 @@ import {useGLTF} from '@react-three/drei';
 
 
 const hackerRoom = (props) => {
-const {scene} = useGLTF('../public/model/hacker_room_-_stylized.glb');
+const {scene} = useGLTF('/public/model/hacker_room.glb');
 
   return (
    <primitive object={scene} {...props} />
